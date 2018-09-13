6bf7e71028c1098ea696d8f3c9cbd08f94d51575
khalid jamal <kh.jamal95@gmail.com>
2018-09-13 21:25:45 +0000

    automatically backed up by learn

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -9,6 +9,7 @@ import React from 'react';
         <div className="content">
           <a className="header">{name} {gender === 'male' ? '♂' : '♀'}</a>
           <div className="meta">
+<<<<<<< HEAD
             <span className="date">{type}</span>
           </div>
           <div className="description">
@@ -26,10 +27,30 @@ import React from 'react';
             >
               Adopt pet
             </button>
+=======
+            <span className="date">Pet type {this.props.type}</span>
+          </div>
+          <div className="description">
+            <p>Age: {this.props.age}</p>
+            <p>Weight: {this.props.weight}</p>
+          </div>
+        </div>
+        <div className="extra content">
+          {this.props.isAdopted 
+          ?  <button className="ui disabled button">Already adopted</button>
+          :<button onClick={this.props.onAdoptPet()}
+          className="ui primary button">Adopt pet</button>
+            
+>>>>>>> 6e3d06efd1da377bb2501dadb261c0499a03a35a
           }
         </div>
       </div>
     );
   }
 }
+<<<<<<< HEAD
  export default Pet; 
+=======
+
+export default Pet;
+>>>>>>> 6e3d06efd1da377bb2501dadb261c0499a03a35a
